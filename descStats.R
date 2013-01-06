14446571725de9ac51e846a16c3cf0001b5d5182
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-01-07 01:11:40 +0700

    Fix

--- descStats.R
+++ descStats.R
@@ -1,11 +1,14 @@
 descStats <- function(x, stats = c("n", "min", "max", "range", "mean", "median", "sd")) {
   n <- function(x, ...) sum(!is.na(x), ...)
   range <- function(x, ...) max(x, ...) - min(x, ...)
-  if (is.vector(x)) {
+  fun <- function(x) {
     result <- vapply(stats, function(z) eval(call(z, x, na.rm=TRUE)), FUN.VALUE=numeric(1))
   }
+  if (is.vector(x)) {
+    result <- fun(x)
+  }
   if (is.matrix(x) || is.data.frame(x)) {
-    result <- t(apply(x, 2, function(x) vapply(stats, function(z) eval(call(z, x, na.rm=TRUE)), FUN.VALUE=numeric(1))))
+    result <- t(apply(x, 2, fun))
   }
   return(result)
 }
