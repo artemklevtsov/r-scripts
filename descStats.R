b7a8684781ed256846e37016caf6591e9ae37d08
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-01-06 13:32:09 +0700

    Транспонировал вывод для таблиц и матриц

--- descStats.R
+++ descStats.R
@@ -5,7 +5,7 @@ descStats <- function(x, stats = c("n", "min", "max", "range", "mean", "median",
     result <- vapply(stats, function(z) eval(call(z, x, na.rm=TRUE)), FUN.VALUE=numeric(1))
   }
   if (is.matrix(x) || is.data.frame(x)) {
-    result <- apply(x, 2, function(x) vapply(stats, function(z) eval(call(z, x, na.rm=TRUE)), FUN.VALUE=numeric(1)))
+    result <- t(apply(x, 2, function(x) vapply(stats, function(z) eval(call(z, x, na.rm=TRUE)), FUN.VALUE=numeric(1))))
   }
   return(result)
 }
