87c6c42cb61ad2f822499100014e816b6bfe593f
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-01-08 05:07:36 +0700

    Добавил функционал

--- descStats.R
+++ descStats.R
@@ -1,4 +1,4 @@
-descStats <- function(x, skew = FALSE, byrow = FALSE) {
+descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE) {
   x <- as.matrix(x)
   if(byrow) x <- t(x)
   stats <- c("n", "min", "max", "range", "mean", "median", "sd")
@@ -6,10 +6,14 @@ descStats <- function(x, skew = FALSE, byrow = FALSE) {
     library(moments)
     stats <- c(stats, "skewness", "kurtosis")
   }
-  n <- function(x, ...) sum(!is.na(x), ...)
-  range <- function(x, ...) max(x, ...) - min(x, ...)
+  if (!is.null(trim)) {
+    stats <- append(stats, "trimmed", which(stats == "mean"))
+    trimmed <- function(x, na.rm=FALSE) mean(x, na.rm=na.rm, trim=trim)
+  }
+  n <- function(x, na.rm=FALSE) sum(!is.na(x), na.rm=na.rm)
+  range <- function(x, na.rm=FALSE) max(x, na.rm=na.rm) - min(x, na.rm=na.rm)
   describe <- function(x) {
-    result <- vapply(stats, function(fun) eval(call(fun, x, na.rm=TRUE)), FUN.VALUE=numeric(1))
+    result <- vapply(stats, function(fun) eval(call(fun, x, na.rm=na.rm)), FUN.VALUE=numeric(1))
   }
   out <- t(vapply(seq_len(ncol(x)), function(i) describe(x[,i]), FUN.VALUE=numeric(length(stats))))
   return(out)
