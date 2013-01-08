2254875e5dddaea7d797164c6cd196d2a0fd5d65
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-01-08 20:07:01 +0700

    Оптимизация кода

--- descStats.R
+++ descStats.R
@@ -1,20 +1,47 @@
-descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE) {
-  x <- as.matrix(x)
+descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE, digits = getOption("digits")) {
+  fun <- function(x) {
+    if(na.rm) x <- x[!is.na(x)]
+    n <- length(x)
+    mean <- sum(x)/n # mean
+    dev <- x-mean
+    dev_s2 <- sum(dev^2L)
+    sd <- sqrt(dev_s2/(n-1)) # sd
+    se <- sqrt(sd/n) # se
+    median <- local({ # median
+      half <- (n + 1L)%/%2L
+      if (n%%2L == 1L) 
+        .Internal(sort(x, partial = half))[half]
+      else {
+        mean(.Internal(sort(x, partial = half + 0L:1L))[half + 0L:1L])
+      }
+    })
+    min <- min(x)
+    max <- max(x)
+    range <- max - min
+    stats <- c(n, mean, se, sd, median, min, max, range)
+    if (!is.null(trim)) {
+      trimmed <- mean.default(x, trim=trim)
+      stats <- append(stats, trimmed, 2)
+    }
+    if (skew) {      
+      skewness <- (sum(dev^3L)/n) / (dev_s2/n)^1.5
+      stats <- c(stats, skewness)
+      
+      kurtosis <- n * sum(dev^4L) / (dev_s2^2L)
+      stats <- c(stats, kurtosis)
+    }
+    stats
+  }
+  if (!is.matrix(x)) x <- as.matrix(x)
   if(byrow) x <- t(x)
-  stats <- c("n", "min", "max", "range", "mean", "median", "sd")
-  if (skew) {
-    library(moments)
-    stats <- c(stats, "skewness", "kurtosis")
+  nstats <- c("n", "mean", "se", "sd", "median", "min", "max", "range")
+  if (skew) nstats <- c(nstats, "skewness", "kurtosis")
+  if (!is.null(trim)) nstats <- append(nstats, "trimmed", which(nstats == "mean"))
+  
+  #out <- t(vapply(seq_len(ncol(x)), function(i) fun(x[,i]), FUN.VALUE=numeric(nstats)))
+  out <- matrix(0.0, ncol = length(nstats), nrow = ncol(x), dimnames=list(colnames(x), nstats))
+  for(i in 1:ncol(x)) {
+    out[i,] <- fun(x[,i])    
   }
-  if (!is.null(trim)) {
-    stats <- append(stats, "trimmed", which(stats == "mean"))
-    trimmed <- function(x, na.rm=FALSE) mean(x, na.rm=na.rm, trim=trim)
-  }
-  n <- function(x, na.rm=FALSE) sum(!is.na(x), na.rm=na.rm)
-  range <- function(x, na.rm=FALSE) max(x, na.rm=na.rm) - min(x, na.rm=na.rm)
-  describe <- function(x) {
-    result <- vapply(stats, function(fun) eval(call(fun, x, na.rm=na.rm)), FUN.VALUE=numeric(1))
-  }
-  out <- t(vapply(seq_len(ncol(x)), function(i) describe(x[,i]), FUN.VALUE=numeric(length(stats))))
-  return(out)
+  round(out, digits=digits)
 }
