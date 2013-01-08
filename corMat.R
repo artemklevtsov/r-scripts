fc28f03ada3d230e81c751a076c87b72392449a6
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-01-09 00:23:08 +0700

    Небольшие правки

--- corMat.R
+++ corMat.R
@@ -1,8 +1,9 @@
-corMat <- function(x, digits=getOption("digits"), ...) {
+corMat <- function(x, digits=getOption("digits"), method = c("pearson", "kendall", "spearman")) {
   ifelse (is.data.frame(x),  mat <- data.matrix(x), mat <- x)
-  cor.mat <- cor(mat, ...)
+  method <- match.arg(method)
+  cor.mat <- cor(mat, method = method)
   index <- combn(ncol(mat), 2)
-  pvals <- mapply(function(x, y) cor.test(mat[, x], mat[, y], ...)$p.value, index[1, ], index[2, ])
+  pvals <- mapply(function(x, y) cor.test(mat[, x], mat[, y], method = method)$p.value, index[1, ], index[2, ])
   cor.mat[lower.tri(cor.mat)] <- pvals
   diag(cor.mat) <- NA
   cor.mat <- round(cor.mat, digits=digits)
