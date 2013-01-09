acfb4ebe1291fbbcf46b7fc650aa4a8481f74a26
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-01-09 08:36:08 +0700

    Проверка вводных данных

--- corMat.R
+++ corMat.R
@@ -1,4 +1,6 @@
-corMat <- function(x, digits=getOption("digits"), method = c("pearson", "kendall", "spearman")) {
+corMat <- function(x, method = c("pearson", "kendall", "spearman"), digits=getOption("digits")) {
+  stopifnot(is.matrix(x) || is.data.frame(x))
+  stopifnot(ncol(x) > 1L)
   ifelse (is.data.frame(x),  mat <- data.matrix(x), mat <- x)
   method <- match.arg(method)
   cor.mat <- cor(mat, method = method)
@@ -6,6 +8,5 @@ corMat <- function(x, digits=getOption("digits"), method = c("pearson", "kendall
   pvals <- mapply(function(x, y) cor.test(mat[, x], mat[, y], method = method)$p.value, index[1, ], index[2, ])
   cor.mat[lower.tri(cor.mat)] <- pvals
   diag(cor.mat) <- NA
-  cor.mat <- round(cor.mat, digits=digits)
-  return(cor.mat)
+  round(cor.mat, digits=digits)
 }
