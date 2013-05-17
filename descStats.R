7ea79222aa6e7a8a7bbaededf4c81119596437eb
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-05-17 19:39:25 +0700

    Check numeric

--- descStats.R
+++ descStats.R
@@ -1,18 +1,19 @@
 descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE, digits = getOption("digits")) {
   fun <- function(x) {
-    if(na.rm) x <- x[!is.na(x)]
+    stopifnot(is.numeric(x))
+    if (na.rm) x <- x[!is.na(x)]
     n <- length(x)
     mean <- sum(x) / n
     dev <- x - mean
-    dev_s2 <- sum(dev^2L)
-    sd <- sqrt(dev_s2 / (n-1))
+    dev_ss <- sum(dev^2L)
+    sd <- sqrt(dev_ss / (n-1))
     se <- sqrt(sd / n)
     median <- local({
       half <- (n + 1L)%/%2L
       if (n%%2L == 1L) 
-        .Internal(sort(x, partial = half))[half]
+        return(.Internal(sort(x, partial = half))[half])
       else {
-        .Internal(mean(.Internal(sort(x, partial = half + 0L:1L))[half + 0L:1L]))
+        return(.Internal(mean(.Internal(sort(x, partial = half + 0L:1L))[half + 0L:1L])))
       }
     })
     min <- min(x)
@@ -20,20 +21,24 @@ descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE,
     range <- max - min
     stats <- c(n, mean, se, sd, median, min, max, range)
     if (!is.null(trim)) {
-      trimmed <- local({
-        lo <- floor(n * trim) + 1
-        hi <- n + 1 - lo
-        x <- .Internal(sort(x, partial = unique(c(lo, hi))))[lo:hi]
-        sum(x) / n
-      })
+      if (trim >= 0.5)
+        trimmed <- median
+      else {
+        trimmed <- local({
+          lo <- floor(n * trim) + 1
+          hi <- n + 1 - lo
+          x <- .Internal(sort(x, partial = unique(c(lo, hi))))[lo:hi]
+          return(sum(x) / n)
+        })
+      }
       stats <- append(stats, trimmed, 2)
     }
     if (skew) {      
-      skewness <- (sum(dev^3L)/n) / (dev_s2/n)^1.5
-      kurtosis <- n * sum(dev^4L) / (dev_s2^2L)
+      skewness <- (sum(dev^3L)/n) / (dev_ss/n)^1.5
+      kurtosis <- n * sum(dev^4L) / (dev_ss^2L)
       stats <- c(stats, skewness, kurtosis)
     }
-    stats
+    return(stats)
   }
   if (!is.matrix(x)) x <- as.matrix(x)
   if(byrow) x <- t.default(x)
@@ -45,5 +50,5 @@ descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE,
       lapply(seq_len(ncol(x)), function(i) fun(x[,i]))
     ), ncol = length(nstats), nrow = ncol(x),
     dimnames = list(colnames(x), nstats), byrow=TRUE)
-  round(out, digits=digits)
+  return(round(out, digits=digits))
 }
