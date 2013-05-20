04f4bfc051e7abf1ffb38e9806ace5dbd30ea06a
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2013-05-20 18:55:52 +0700

    Small fixes

--- descStats.R
+++ descStats.R
@@ -7,16 +7,13 @@ descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE,
     mean <- sum(x) / n
     dev <- x - mean
     dev_ss <- sum(dev^2L)
-    sd <- sqrt(dev_ss / (n-1))
+    sd <- sqrt(dev_ss / (n - 1))
     se <- sqrt(sd / n)
-    median <- local({
-      half <- (n + 1L)%/%2L
-      if (n%%2L == 1L) 
-        return(.Internal(sort(x, partial = half))[half])
-      else {
-        return(.Internal(mean(.Internal(sort(x, partial = half + 0L:1L))[half + 0L:1L])))
-      }
-    })
+    half <- (n + 1L)%/%2L
+    if (n %% 2L == 1L)
+      median <- .Internal(sort(x, partial = half))[half]
+    else
+      median <- .Internal(mean(.Internal(sort(x, partial = half + 0L:1L))[half + 0L:1L]))
     min <- min(x)
     max <- max(x)
     range <- max - min
@@ -34,8 +31,8 @@ descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE,
       }
       stats <- append(stats, trimmed, 2)
     }
-    if (skew) {      
-      skewness <- (sum(dev^3L)/n) / (dev_ss/n)^1.5
+    if (skew) {
+      skewness <- (sum(dev^3L) / n) / (dev_ss / n)^1.5
       kurtosis <- n * sum(dev^4L) / (dev_ss^2L)
       stats <- c(stats, skewness, kurtosis)
     }
@@ -43,17 +40,18 @@ descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE,
   }
   if (!is.matrix(x))
     x <- as.matrix(x)
-  if(byrow)
+  if (byrow)
     x <- t.default(x)
-  nstats <- c("n", "mean", "se", "sd", "median", "min", "max", "range")
+  stat.nanmes <- c("n", "mean", "se", "sd", "median", "min", "max", "range")
   if (skew)
-    nstats <- c(nstats, "skewness", "kurtosis")
+    stat.nanmes <- c(stat.nanmes, "skewness", "kurtosis")
   if (!is.null(trim))
-    nstats <- append(nstats, "trimmed", 2)
-  out <- matrix(
-    unlist(
-      lapply(seq_len(ncol(x)), function(i) fun(x[,i]))
-    ), ncol = length(nstats), nrow = ncol(x),
-    dimnames = list(colnames(x), nstats), byrow=TRUE)
-  return(round(out, digits=digits))
+    stat.nanmes <- append(stat.nanmes, "trimmed", 2)
+  var.names <- colnames(x)
+  result <- matrix(
+    vapply(seq_len(ncol(x)), function(i) fun(x[,i]),
+           FUN.VALUE=numeric(length(stat.nanmes))),
+    ncol = length(stat.nanmes), nrow = ncol(x),
+    dimnames = list(var.names, stat.nanmes), byrow=TRUE)
+  return(round(result, digits=digits))
 }
