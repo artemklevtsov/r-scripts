a909864bd746c9c5de2134dae30828d08adc975c
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-01-29 17:42:55 +0700

    Отступы

--- descStats.R
+++ descStats.R
@@ -1,65 +1,65 @@
 descStats <- function(x, na.rm = TRUE, trim = NULL, skew = FALSE, byrow = FALSE, digits = getOption("digits")) {
-  fun <- function(x) {
-    stopifnot(is.numeric(x))
-    if (na.rm) x <- x[!is.na(x)]
-    stats <- numeric(8)
-    n <- length(x)
-    mean <- sum(x) / n
-    dev <- x - mean
-    dev_s2 <- sum(dev^2L)
-    sd <- sqrt(dev_s2 / (n - 1))
-    se <- sqrt(sd / n)
-    half <- (n + 1L) %/% 2L
-    if (n %% 2L == 1L)
-      median <- sort(x, partial = half)[half]
-    else
-      median <- sum(sort(x, partial = half + 0L:1L)[half + 0L:1L]) / 2L
-    min <- min(x)
-    max <- max(x)
-    range <- max - min
-    stats <- c(n, mean, se, sd, median, min, max, range)
-    if (!is.null(trim)) {
-      if (trim >= 0.5)
-        trimmed <- median
-      else {
-        trimmed <- local({
-          lo <- floor(n * trim) + 1
-          hi <- n + 1 - lo
-          x <- sort(x, partial = unique(c(lo, hi)))[lo:hi]
-          return(sum(x) / n)
-        })
-      }
-      stats <- append(stats, trimmed, 2)
+    fun <- function(x) {
+        stopifnot(is.numeric(x))
+        if (na.rm) x <- x[!is.na(x)]
+        stats <- numeric(8)
+        n <- length(x)
+        mean <- sum(x) / n
+        dev <- x - mean
+        dev_s2 <- sum(dev^2L)
+        sd <- sqrt(dev_s2 / (n - 1))
+        se <- sqrt(sd / n)
+        half <- (n + 1L) %/% 2L
+        if (n %% 2L == 1L)
+            median <- sort(x, partial = half)[half]
+        else
+            median <- sum(sort(x, partial = half + 0L:1L)[half + 0L:1L]) / 2L
+        min <- min(x)
+        max <- max(x)
+        range <- max - min
+        stats <- c(n, mean, se, sd, median, min, max, range)
+        if (!is.null(trim)) {
+            if (trim >= 0.5)
+                trimmed <- median
+            else {
+                trimmed <- local({
+                    lo <- floor(n * trim) + 1
+                    hi <- n + 1 - lo
+                    x <- sort(x, partial = unique(c(lo, hi)))[lo:hi]
+                    return(sum(x) / n)
+                })
+            }
+            stats <- append(stats, trimmed, 2)
+        }
+        if (skew) {
+            skewness <- (sum(dev^3L) / n) / (dev_ss / n)^1.5
+            kurtosis <- n * sum(dev^4L) / (dev_ss^2L)
+            stats <- c(stats, skewness, kurtosis)
+        }
+        return(stats)
     }
-    if (skew) {
-      skewness <- (sum(dev^3L) / n) / (dev_ss / n)^1.5
-      kurtosis <- n * sum(dev^4L) / (dev_ss^2L)
-      stats <- c(stats, skewness, kurtosis)
+    cn <- colnames(x)
+    n.vars <- ncol(x)
+    if (is.data.frame(x)) {
+        for (i in seq_len(n.vars)) {
+            if (!is.numeric(x[, i]) || is.factor(x[, i])) {
+                x <- x[, -i]
+                warning(paste("Variable \"", colnames(x)[i], "\"was droped from data.frame."))
+            }
+        }
     }
-    return(stats)
-  }
-  cn <- colnames(x)
-  n.vars <- ncol(x)
-  if (is.data.frame(x)) {
-    for (i in seq_len(n.vars)) {
-      if (!is.numeric(x[, i]) || is.factor(x[, i])) {
-        x <- x[, -i]
-        warning(paste("Variable \"", colnames(x)[i], "\"was removed from data.frame."))
-      }
-    }
-  }
-  if (!is.matrix(x))
-    x <- as.matrix(x)
-  if (byrow)
-    x <- t.default(x)
-  sn <- c("n", "mean", "se", "sd", "median", "min", "max", "range")
-  if (skew)
-    sn <- c(stat.nanmes, "skewness", "kurtosis")
-  if (!is.null(trim))
-    sn <- append(stat.nanmes, "trimmed", 2)
-  result <- matrix(numeric(1), ncol = length(sn), nrow = n.vars,
-                   dimnames = list(cn, sn), byrow = TRUE)
-  for (i in seq_len(n.vars))
-    result[i, ] <- fun(x[, i])
-  return(signif(result, digits = digits))
+    if (!is.matrix(x))
+        x <- as.matrix(x)
+    if (byrow)
+        x <- t.default(x)
+    sn <- c("n", "mean", "se", "sd", "median", "min", "max", "range")
+    if (skew)
+        sn <- c(stat.nanmes, "skewness", "kurtosis")
+    if (!is.null(trim))
+        sn <- append(stat.nanmes, "trimmed", 2)
+    result <- matrix(numeric(1), ncol = length(sn), nrow = n.vars,
+                     dimnames = list(cn, sn), byrow = TRUE)
+    for (i in seq_len(n.vars))
+        result[i, ] <- fun(x[, i])
+    return(signif(result, digits = digits))
 }
--- proftable.R
+++ proftable.R
@@ -1,52 +1,52 @@
 proftable <- function(x, ...) {
-  UseMethod("proftable")
+    UseMethod("proftable")
 }
 
 proftable.default <- function(filename, lines = 10) {
-  con <- file(filename, "rt")
-  on.exit(close(con))
-  profdata <- readLines(con)
-  interval <- as.numeric(strsplit(profdata[1L], "=")[[1L]][2L]) / 1e+06
-  profdata <- profdata[-1L]
-  filelines <- grep("^#File [0-9]+: ", profdata)
-  if (length(filelines)) {
-    files <- profdata[filelines]
-    filenums <- as.integer(gsub("^#File ([0-9]+): .*", "\\1", files))
-    filenames <- gsub("^#File [0-9]+: ", "", files)
-    profdata <- profdata[-filelines]
-  }
-  ncalls <- length(profdata)
-  total.time <- interval * ncalls
-  profdata <- gsub("\\\"| $", "", profdata)
-  stacktable <- as.data.frame(table(profdata) / ncalls * 100, stringsAsFactors = FALSE)
-  calls <- strsplit(stacktable$profdata, " ")
-  calls <- lapply(calls, function(x) rev(x))
-  min.len <- min(vapply(calls, length, FUN.VALUE = numeric(1)))
-  parent.call <- unlist(lapply(seq_len(min.len), function(i) Reduce(intersect, lapply(unique(calls), "[[", i))))
-  calls <- lapply(calls, function(x) setdiff(x, parent.call))
-  stacktable$profdata <- vapply(calls, function(x) paste(x, collapse = " > "), FUN.VALUE = character(1))
-  stacktable <- stacktable[order(stacktable$Freq[], decreasing = TRUE), 2:1]
-  colnames(stacktable) <- c("PctTime", "Call")
-  rownames(stacktable) <- NULL
-  stacktable <- head(stacktable, lines)
-  if (length(parent.call) > 0)
-    parent.call <- paste(parent.call, collapse = " > ")
-  else
-    parent.call <- "None"
-  frac <- sum(stacktable$PctTime)
-  result <- list(data = profdata, table = stacktable, parent.call = parent.call, interval = interval, total.time = total.time, files = filenames, total.pct.time = frac)
-  class(result) <- "proftable"
-  return(result)
+    con <- file(filename, "rt")
+    on.exit(close(con))
+    profdata <- readLines(con)
+    interval <- as.numeric(strsplit(profdata[1L], "=")[[1L]][2L]) / 1e+06
+    profdata <- profdata[-1L]
+    filelines <- grep("^#File [0-9]+: ", profdata)
+    if (length(filelines)) {
+        files <- profdata[filelines]
+        filenums <- as.integer(gsub("^#File ([0-9]+): .*", "\\1", files))
+        filenames <- gsub("^#File [0-9]+: ", "", files)
+        profdata <- profdata[-filelines]
+    }
+    ncalls <- length(profdata)
+    total.time <- interval * ncalls
+    profdata <- gsub("\\\"| $", "", profdata)
+    stacktable <- as.data.frame(table(profdata) / ncalls * 100, stringsAsFactors = FALSE)
+    calls <- strsplit(stacktable$profdata, " ")
+    calls <- lapply(calls, function(x) rev(x))
+    min.len <- min(vapply(calls, length, FUN.VALUE = numeric(1)))
+    parent.call <- unlist(lapply(seq_len(min.len), function(i) Reduce(intersect, lapply(unique(calls), "[[", i))))
+    calls <- lapply(calls, function(x) setdiff(x, parent.call))
+    stacktable$profdata <- vapply(calls, function(x) paste(x, collapse = " > "), FUN.VALUE = character(1))
+    stacktable <- stacktable[order(stacktable$Freq[], decreasing = TRUE), 2:1]
+    colnames(stacktable) <- c("PctTime", "Call")
+    rownames(stacktable) <- NULL
+    stacktable <- head(stacktable, lines)
+    if (length(parent.call) > 0)
+        parent.call <- paste(parent.call, collapse = " > ")
+    else
+        parent.call <- "None"
+    frac <- sum(stacktable$PctTime)
+    result <- list(data = profdata, table = stacktable, parent.call = parent.call, interval = interval, total.time = total.time, files = filenames, total.pct.time = frac)
+    class(result) <- "proftable"
+    return(result)
 }
  
 print.proftable <- function(x) {
-  print(x$table, row.names=FALSE, right=FALSE, digits=3)
-  cat("\nFiles:\n")
-  cat(paste(x$files, collapse="\n"))
-  cat("\n\n")
-  cat(paste("Parent Call:", x$parent.call))
-  cat("\n\n")
-  cat(paste("Total Time:", x$total.time, "seconds"))
-  cat("\n")
-  cat(paste0("Percent of run time represented: ", format(x$total.pct.time, digits = 3), "%"))
+    print(x$table, row.names=FALSE, right=FALSE, digits=3)
+    cat("\nFiles:\n")
+    cat(paste(x$files, collapse="\n"))
+    cat("\n\n")
+    cat(paste("Parent Call:", x$parent.call))
+    cat("\n\n")
+    cat(paste("Total Time:", x$total.time, "seconds"))
+    cat("\n")
+    cat(paste0("Percent of run time represented: ", format(x$total.pct.time, digits = 3), "%"))
 }
